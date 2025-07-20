ecd13e203a300fc4dc2668849cd56e52000d2e39
OĞUZHAN SEÇGEL <osecgel@hotmail.com>
2025-07-20 17:40:17 +0300

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# grpc-with-spring-boot
+# Grpc with Spring Boot
+
+### Client Api Response
+#### GetAllProduct
+```bash
+curl -plaintext -H "Content-Type: application/json" -d '{}' "http://localhost:8081/api/products"
+```
+
+### Product Service Grpcurl
+```bash 
+grpcurl.exe -plaintext -d "{}" localhost:1111 com.shop.product.ProductService/GetAllProduct
+```
--- product-service/src/main/java/com/shop/product_service/GrpcConfig.java
+++ product-service/src/main/java/com/shop/product_service/GrpcConfig.java
@@ -3,6 +3,7 @@ package com.shop.product_service;
 import com.shop.product_service.service.ProductService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ public class GrpcConfig {
     public Server grpcServer(ProductService productService) throws IOException {
         Server server = ServerBuilder.forPort(port)
                 .addService(productService)
+                .addService(ProtoReflectionService.newInstance())
                 .build()
                 .start();
 
